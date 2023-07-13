58d24fd2884b9b6c5b6742c8db0d96d625874323
shady2000shady <shady121288@gmail.com>
2023-07-13 14:59:24 -0700

    5th commit use puts func.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints using a func.
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
