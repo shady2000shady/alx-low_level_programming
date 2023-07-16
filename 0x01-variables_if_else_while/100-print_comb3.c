18262a3aa51766e10589454747795720897b4e53
shady2000shady <shady121288@gmail.com>
2023-07-15 19:04:32 -0700

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: print all possible different combinations of two digits
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
+			if (d1 != d2 && d1 < d2)
+			{
+				putchar(d1 + 48);
+				putchar(d2 + 48);
+
+				if (d1 + d2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
