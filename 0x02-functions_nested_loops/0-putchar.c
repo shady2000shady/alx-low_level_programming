c363f7cbd0ae0980d6c4437095d450526d764dee
shady2000shady <shady121288@gmail.com>
2023-07-19 16:06:04 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entery point
+ *
+ * Description: func. _putchar prints putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char stri[] = "_putchar";
+	int cha;
+
+	for (cha = 0; cha < 8; cha++)
+		_putchar(stri[ch]);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is Functions_nested_loops
