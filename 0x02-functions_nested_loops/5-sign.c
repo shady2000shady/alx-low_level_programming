e27e0dc6034b2133b776e90b1ed25d59007685e9
shady2000shady <shady121288@gmail.com>
2023-07-20 09:26:53 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * prints_sign - prints the sign of a number
- * @n: the int to check
+ * Discription: func. that prints sign of char.
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
