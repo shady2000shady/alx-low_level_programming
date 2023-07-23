d479580583a2e9eae9fc92af0d6ad170ac7bb130
shady2000shady <shady121288@gmail.com>
2023-07-23 07:36:10 -0700

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,20 +2,20 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: is number of times the _ char
- * shloud be prited out
+ * @n: number of times the character _ should be printed
  */
 
 void print_line(int n)
 {
-	int chr;
-
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (chr = 0; chr <= n; chr++)
+		int i;
+
+		for (i = 1; i <= n; i++)
 			_putchar('_');
 		_putchar('\n');
 	}
 }
+
